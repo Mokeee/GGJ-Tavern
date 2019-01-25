8c6e5a196142f11aa9ff38eb491e0234b20bec4a
Dario <dario.vandenboom@arcor.de>
2019-01-25 20:56:40 +0100

    Classes for dialog system

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Need
+{
+    FOO
+}
+
+public enum Character
+{
+    FOO
+}
+
+public class NPC
+{
+    private LinkedList<Need> Needs { get; }
+    private LinkedList<Need> SpecialNeeds { get; }
+
+    private float ComfortLevel { get; }
+    private Character Character { get; }
+
+    public NPC(Character character, LinkedList<Need> needs)
+    {
+        this.Needs = needs;
+        this.SpecialNeeds = new LinkedList<Need>();
+        this.Character = character;
+    }
+
+
+    public void AddSpecialNeed(Need need)
+    {
+        SpecialNeeds.AddLast(need);
+    }
+
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question
+{
+
+    private Character Type { get; }
+    private string Text { get; }
+    private Answer[] Answers;
+
+
+    public Answer GetAnswer()
+    {
+
+    }
+
+}
