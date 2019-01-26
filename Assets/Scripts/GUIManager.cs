d9d8ffeb229c31215a22c819492b6af99f857c57
Student <student@bo-edit-08.bochum.sae>
2019-01-26 16:07:21 +0100

    Dialog System

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIManager : MonoBehaviour
+{
+    public static GUIManager Instance;
+
+
+    public void Start()
+    {
+        Instance = this;
+    }
+
+
+    public void DisplayQuestions(List<string> questions)
+    {
+        //TODO
+    }
+
+
+    public void DisplaySnippet(string text)
+    {
+        //TODO
+    }
+
+
+    public void HideDialog()
+    {
+        //TODO
+    }
+}
