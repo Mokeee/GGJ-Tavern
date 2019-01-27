1f135811457bc41d62857441af0c0c1f0efde496
Dario <dario.vandenboom@arcor.de>
2019-01-27 14:45:25 +0100

    Candle animation

---
+++ Assets/Scripts/TorchAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchAnimation : MonoBehaviour
+{
+    public Light Light;
+    public SpriteRenderer Flame;
+
+    public float Amplitude = 1;
+    public float Frequency = 1;
+
+    public Vector3 Weights = new Vector3(1, 1, 1);
+
+    private Vector3 Scale;
+    private float Intensity;
+    private float Distance;
+
+
+    public void Start()
+    {
+        this.Scale = Flame.transform.localScale;
+        this.Intensity = Light.intensity;
+        this.Distance = Light.range;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float factor = (1 + Amplitude * Mathf.Sin(Time.time * Frequency));
+        Vector3 weightedFactor = factor * Weights;
+        weightedFactor.Scale(Scale);
+
+        Flame.transform.localScale = weightedFactor;
+        Light.intensity = factor * Intensity;
+        Light.range = factor * Distance;
+    }
+}
