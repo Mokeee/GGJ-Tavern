f6f1bf588c23382221249316de278936fea6d091
Dario <dario.vandenboom@arcor.de>
2019-01-27 12:54:13 +0100

    Added NPCMover for entry a nd exit animation

---
+++ Assets/Scripts/NPCMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMover : MonoBehaviour
+{
+    public Vector3 TargetPosition;
+
+    public float Duration = 1;
+
+    public void Start()
+    {
+        MoveToTargetPosition();
+    }
+
+    public void MoveToTargetPosition()
+    {
+        StartCoroutine(MoveAnimation());
+    }
+
+    private IEnumerator MoveAnimation()
+    {
+        Vector3 startPosition = transform.localPosition;
+
+        float startTime = Time.time;
+        float timePassed = 0f;
+
+        while ((timePassed = Time.time - startTime) < Duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, TargetPosition, timePassed / Duration);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
